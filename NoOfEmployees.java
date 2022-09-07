a8ae2e95d8d3062cee7163b2bd7fa255c9879e5d
shubhb4u <shubhammazumdar94@gmail.com>
2022-09-07 14:54:20 -0500

    d

--- NoOfEmployees.java
+++ NoOfEmployees.java
@@ -6,8 +6,28 @@ class Main {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        
-        //write your code here
+        HashMap<String, HashSet<String>> tree = new HashMap<>();
+    
+        String ceo ="";
+        for (int i = 0 ; i<n ; i++) {
+            String emp = scn.next();
+            String manager = scn.next();
+    
+            if (manager.equals(emp)) {
+                ceo = emp;
+            } else {
+                HashSet<String> empList = tree.get(manager);
+                //Using IsEmpty() throws NPE because no HashSet is present for the manager yet.
+                if (empList == null) {
+                    empList = new HashSet<>();
+                    empList.add(emp);
+                    tree.put(manager, empList);
+                } else {
+                    empList.add(emp);
+                }
+            }
+        }
+        System.out.println(tree);
     }
     
 }
