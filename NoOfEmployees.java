b09e9a665ed32abef70415c6a76e8628beab1a54
shubhb4u <shubhammazumdar94@gmail.com>
2022-09-07 09:42:31 -0500

    No of employees

---
+++ NoOfEmployees.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        //write your code here
+    }
+    
+}
