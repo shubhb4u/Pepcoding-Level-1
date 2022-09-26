534c13faff0664aab7302e79beb617f8fba96512
shubhb4u <shubhammazumdar94@gmail.com>
2022-09-26 15:33:27 -0500

    Binary tree cameras

---
+++ BinaryTree/BinaryTreeCameras.java
@@ -0,0 +1,58 @@
+package BinaryTree;
+import java.util.*;
+
+public class BinaryTreeCameras {
+    
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    
+        TreeNode() {
+        }
+    
+        TreeNode(int val) {
+            this.val = val;
+        }
+    
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    static int count;
+    
+    public int minCameraCover(TreeNode root) {
+        
+        count = 0;
+        int res = status(root);
+        //If there is just one node given it will pass -1.
+        if(res == -1){
+            return count++;
+        }
+        return count;
+        
+    }
+    public int status(TreeNode root) {
+        
+        if(root == null){
+            return 1;
+        }
+        
+        int lc = status(root.left);
+        int rc = status(root.right);
+        
+        if(lc == -1 || rc == -1){
+            count++;
+            return 0;
+        }
+        else if(lc == 0 || rc == 0){
+            
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+}
--- BinaryTree/LevelOrderTraversal.java
+++ BinaryTree/LevelOrderTraversal.java
@@ -1,3 +1,4 @@
+package BinaryTree;
 import java.io.*;
 import java.util.*;
 
