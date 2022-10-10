82c0d8fe9a96aa309092b57248a7d066d84d9900
shubhb4u <shubhammazumdar94@gmail.com>
2022-10-10 11:41:37 -0500

    BT to DLL

--- BinaryTree/Cameras.java
+++ BinaryTree/Cameras.java
@@ -4,20 +4,13 @@ import java.util.*;
 
 public class Cameras {
     
-    public class TreeNode {
-        int val;
+    public static class TreeNode {
+        int data;
         TreeNode left;
         TreeNode right;
         
-        TreeNode() {
-        }
-        
-        TreeNode(int val) {
-            this.val = val;
-        }
-        
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
+        TreeNode(int data, TreeNode left, TreeNode right) {
+            this.data = data;
             this.left = left;
             this.right = right;
         }
@@ -29,28 +22,26 @@ public class Cameras {
         
         count = 0;
         int res = status(root);
-        //If there is just one node given it will pass -1.
+        //If there is just one TreeNode given it will pass -1.
         if(res == -1){
             return count+1;
         }
         return count;
-        
     }
+    
     public int status(TreeNode root) {
         
-        if(root == null){
+        if (root == null){
             return 1;
         }
-        
         int lc = status(root.left);
         int rc = status(root.right);
         
-        if(lc == -1 || rc == -1){
+        if (lc == -1 || rc == -1){
             count++;
             return 0;
         }
         else if(lc == 0 || rc == 0){
-            
             return 1;
         }
         else{
